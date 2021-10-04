4386d96d41438ca5c0310652dddd52a71c97b604
sachintaw <sachintaware.2551988@gmail.com>
2021-10-04 17:34:16 +0530

    ignoring local test classes

--- src/main/java/com/cybage/assignment/page/sample.java
+++ src/main/java/com/cybage/assignment/page/sample.java
@@ -1,20 +1,10 @@
 package com.cybage.assignment.page;
 
-import com.cybage.assignment.objects.*;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class sample
 {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
+        System.out.println("Hello World");
 
-        String updateQuery="UPDATE \"testSchema1\".\"testTable1\" \n" +
-                "\tSET  marks=89\n" +
-                "\tWHERE student_Name='John Wick';";
-        SqlUtility sql=new SqlUtility();
-        sql.updateTable("CybageDB",updateQuery);
-        sql.tableData("CybageDB","\"testSchema1\".\"testTable1\"",3);
-    }
+         }
 }
