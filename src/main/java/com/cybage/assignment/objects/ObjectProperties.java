eddec19b65e934a180bd6b0f4281ecd35def8657
sachintaw <sachintaware.2551988@gmail.com>
2021-10-14 16:44:57 +0530

    Created YAML file reader utility ObjectProperties

---
+++ src/main/java/com/cybage/assignment/objects/ObjectProperties.java
@@ -0,0 +1,40 @@
+package com.cybage.assignment.objects;
+
+import org.yaml.snakeyaml.Yaml;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Map;
+
+public class ObjectProperties
+{
+    private static String ymlFilePath;
+    private static Map<?,?> map;
+
+    /* To initialize yaml file reader at specified path  & load data into map*/
+    private static String initialize(String objectCall)
+    {
+        try(Reader rd = new FileReader(ymlFilePath);)
+        {
+            Yaml yaml = new Yaml();
+            Map<?,?>property = (Map<?, ?>) yaml.load(rd);
+            map =(Map<?,?>)property.get(objectCall.split("\\.")[0]);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return map.get(objectCall.split("\\.")[1]).toString();
+    }
+
+    /* To set path of desired yaml file to be used */
+    public static void initializeObjectProperties(String yamlPtah)
+    {
+        ymlFilePath=yamlPtah;
+    }
+
+    /* To get object called by specified key */
+    public static String getProperty(String objectCall)
+    {
+       return initialize(objectCall);
+    }
+}
